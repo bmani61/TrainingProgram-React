9850626f94ceb4733789368e3cd7e27ff7eef7bf
bmani61 <chinna.mrb@gmail.com>
2020-09-07 07:01:17 +0530

    fixed warnings of unused components

--- src/components/TrainingProgram/Program/programlist.js
+++ src/components/TrainingProgram/Program/programlist.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
-import Link from "../../link";
+//import Link from "../../link";
 
 const ProgramList = ({ programs }) => {
   const list = programs.map((program) => {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import Route from "./components/route";
+//import Route from "./components/route";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
